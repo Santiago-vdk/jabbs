2d250635e6256ae35d7a5d2856f3e293a7f0a815
Santiago Vargas de Kruijf <santiago.vdk@gmail.com>
2016-02-02 09:53:34 -0600

    Fixed DB migration error

--- jobs/models.py
+++ jobs/models.py
@@ -4,6 +4,14 @@ from django.db import models
 
 from ckeditor.fields import RichTextField
 
+
+
+#Location 
+#class PointOfInterest(models.Model):
+#    name = models.CharField(max_length=100)
+#    position = GeopositionField()
+
+#Tag Model
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
     class Meta:
@@ -14,22 +22,24 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+#Job Type Model - Should be Freelance, Full Time, etc...
 #class Job_Type(models.Model):
 #    name = models.CharField(max_length=24, unique=True)
 #
 #    def __str__(self):
 #        return self.name
 
-
+#School Model
 class School(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
 
-        
+#Company Model        
 class Company(models.Model):
     name = models.CharField(max_length=32)
+    slogan = models.CharField(max_length=32)
     logo = models.URLField(max_length=200)
     video = models.URLField(max_length=200)
     website = models.URLField(max_length=200)
@@ -39,6 +49,7 @@ class Company(models.Model):
     def __str__(self):
         return self.name
 
+#Job Model
 class Job(models.Model):
     school = models.ManyToManyField(School, blank=False)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
@@ -47,10 +58,11 @@ class Job(models.Model):
     exp_date = models.DateTimeField('expiry date')
     job_location = models.CharField(max_length=24)
     job_position = models.CharField(max_length=24)
-    #job_type =  models.ForeignKey(Job_Type, on_delete=models.CASCADE)
+    job_type = models.CharField(max_length=24)
     job_requirements = models.CharField(max_length=200)
     tags = models.ManyToManyField(Tag, blank=True)
     description = RichTextField(blank=True)
 
     def __str__(self):
         return self.name
+
